d062d5985b0fe8ed39199f22d1c59131cf15d088
neiluJ <julien@nitronet.org>
2014-07-23 14:24:34 +0200

    maybe fixed travis build and some documentation

---
+++ Tests/LazyClassDefinitionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Fwk\Di;
+
+/**
+ * Test class for ClassDefinition.
+ */
+class LazyClassDefinitionTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @var ClassDefinition
+     */
+    protected $object;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->object = new LazyClassDefinition('\stdClass');
+        
+    }
+    
+    protected function getContainer()
+    {
+        $container = new Container();
+
+        return $container;
+    }
+    
+   public function testLazyClassLoading()
+   {
+       $class = $this->object->invoke($this->getContainer());
+       $this->assertArrayHasKey('ProxyManager\Proxy\LazyLoadingInterface', class_implements($class));
+   }
+}
